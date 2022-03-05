740d8d4cde271ff5f500edcb01c4bac57aca57a1
Laura Natalia Bello Jiménez <laura.bello@rappi.com>
2022-03-05 18:44:13 -0500

    refactor

--- estado_pedido_actualizado_en_transito/main.py
+++ estado_pedido_actualizado_en_transito/main.py
@@ -3,14 +3,9 @@ import requests
 
 
 def actualizacion_estado_pedido_en_transito(request=None):
-    def update_delivery(payment_info=None):
-        url = f'http://{ORDERS_SERVER}/orders/internal/{payment_info.get_json()["orderId"]}'
-        update_del = requests.post(url, data={'state': 'EN_TRANSITO'})
-        if update_del.status_code != 200:
-            return {"msg": "error while updating delivery"}, 404
-        return update_del
-
-    update_delivery_response = update_delivery(request)
-
-    if update_delivery_response.status_code == 200:
+    url = f'http://{ORDERS_SERVER}/orders/internal/{request.get_json()["orderId"]}'
+    update_del = requests.post(url, data={'state': 'EN_TRANSITO'})
+    if update_del.status_code != 200:
+        return {"msg": "error while updating delivery"}, 404
+    else:
         return {"msg": "succesfull update"}
