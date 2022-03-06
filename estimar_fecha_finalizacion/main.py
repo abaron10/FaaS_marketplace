62331b78e936445977a94a09abf78d0bce5ccf61
William Ravelo M <drummerwilliam@gmail.com>
2022-03-05 19:11:24 -0500

    changes

--- estimar_fecha_finalizacion/main.py
+++ estimar_fecha_finalizacion/main.py
@@ -5,6 +5,7 @@ import env_vars as env
 import uuid
 import json
 import requests
+from datetime import timedelta, date
 
 def estimar_fecha_finalizacion(request):
     try:
@@ -12,7 +13,7 @@ def estimar_fecha_finalizacion(request):
         request_args = request.args
         order_id = request_args['id']
         data = {
-            "state": "ENTREGADA"
+            "state": "PENDIENTE_DE_FINALIZACION"
         }
         order_response = requests.post(
             f'{base_url}/orders/internal/{order_id}',
@@ -22,7 +23,10 @@ def estimar_fecha_finalizacion(request):
         if order_response.status_code != 200:
             raise Exception("Error")
 
+        estimated_fabrication = date.today() + timedelta(days=1)
         # TODO: Notificar al comprador
-        return { "message": "Se notifico al comprador correctamente" }
+        return {
+            "message": "Se notificará al comprador con la fecha estimada de finalización"
+        }
     except Exception as e:
         return "Oops, hubo un error", 400
