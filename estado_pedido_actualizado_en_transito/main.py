d1bbae5ea8c4c6d206c24c61e5fcad4438390c7b
Laura Natalia Bello Jiménez <laura.bello@rappi.com>
2022-03-05 19:11:03 -0500

    set orders server

--- estado_pedido_actualizado_en_transito/main.py
+++ estado_pedido_actualizado_en_transito/main.py
@@ -4,8 +4,8 @@ import requests
 
 def actualizacion_estado_pedido_en_transito(request=None):
     url = f'http://{ORDERS_SERVER}/orders/internal/{request.get_json()["orderId"]}'
-    update_del = requests.post(url, data={'state': 'EN_TRANSITO'})
-    if update_del.status_code != 200:
+    update_delivery = requests.post(url, data={'state': 'EN_TRANSITO'})
+    if update_delivery.status_code != 200:
         return {"msg": "error while updating delivery"}, 404
     else:
         return {"msg": "succesfull update"}
