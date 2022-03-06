2acce99c684cd8274c83964e5056e2340462f64d
William Ravelo M <drummerwilliam@gmail.com>
2022-03-05 22:29:23 -0500

    fix

--- estimar_fecha_finalizacion/main.py
+++ estimar_fecha_finalizacion/main.py
@@ -23,8 +23,21 @@ def estimar_fecha_finalizacion(request):
         if order_response.status_code != 200:
             raise Exception("Error")
 
-        estimated_fabrication = date.today() + timedelta(days=1)
-        # TODO: Notificar al comprador
+        estimated_fabrication = str(date.today() + timedelta(days=1))
+        notification_data = {
+            "notificacion": f"La fecha estimada de tu envio es {estimated_fabrication}",
+            "type": 1,
+            "order_id": str(order_id)
+        }
+        print(notification_data)
+        notificador_response = requests.post(
+            'https://us-central1-miso-339504.cloudfunctions.net/funcion-notificaciones-producir',
+            json=notification_data
+        )
+
+        if notificador_response.status_code != 200:
+            raise Exception("Error")
+
         return {
             "message": "Se notificará al comprador con la fecha estimada de finalización"
         }
